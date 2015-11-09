9321dbdeeaf3abcc7855a12a8a20e20c782bb0b3
linfang <lin.fang@rea-group.com>
2015-11-09 16:34:06 +0800

    add bower and test task

--- gruntfile.js
+++ gruntfile.js
@@ -3,48 +3,53 @@
  * Author: isaac.fang@grapecity.com
  */
 module.exports = function (grunt) {
-    'use strict';
+  'use strict';
 
-    grunt.initConfig({
-        karma: {
-            options: {
-                configFile: 'test/unit/karma.conf.js',
-                reporters: ['progress', 'coverage']
-            },
-            all: {
-                singleRun: true,
-                browsers: ['Chrome', 'Firefox']
-            },
-            debug: {
-                singleRun: false,
-                browsers: ['Chrome']
-            }
-        },
-        protractor: {
-            options: {
-                keepAlive: true,
-                noColor: false,
-                args: {}
-            },
-            all: {
-                options: {
-                    configFile: 'test/e2e/protractor.conf.js',
-                    args: {}
-                }
-            }
-        },
-        shell: {
-            updateWebDriver: {
-                options: {
-                    stdout: true
-                },
-                command: 'node node_modules/grunt-protractor-runner/node_modules/protractor/bin/webdriver-manager update'
-            }
+  grunt.initConfig({
+    karma: {
+      options: {
+        configFile: 'test/unit/karma.conf.js',
+        reporters: ['progress', 'coverage']
+      },
+      silent: {
+        singleRun: true,
+        browsers: ['PhantomJS']
+
+      },
+      all: {
+        singleRun: true,
+        browsers: ['Chrome', 'Firefox']
+      },
+      debug: {
+        singleRun: false,
+        browsers: ['Chrome']
+      }
+    },
+    protractor: {
+      options: {
+        keepAlive: true,
+        noColor: false,
+        args: {}
+      },
+      all: {
+        options: {
+          configFile: 'test/e2e/protractor.conf.js',
+          args: {}
         }
-    });
+      }
+    },
+    shell: {
+      updateWebDriver: {
+        options: {
+          stdout: true
+        },
+        command: 'node node_modules/grunt-protractor-runner/node_modules/protractor/bin/webdriver-manager update'
+      }
+    }
+  });
 
-    grunt.loadNpmTasks('grunt-karma');
-    grunt.loadNpmTasks('grunt-shell');
-    grunt.loadNpmTasks('grunt-protractor-runner');
+  grunt.loadNpmTasks('grunt-karma');
+  grunt.loadNpmTasks('grunt-shell');
+  grunt.loadNpmTasks('grunt-protractor-runner');
 }
 ;
